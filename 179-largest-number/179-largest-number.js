23d05269d06d011d37b7a28ec2dae80ba8af16f1
傅浩南 <fhnstephen@gmail.com>
2015-04-13 22:33:37 +0800

    Add 179.

---
+++ 179-largest-number/179-largest-number.js
@@ -0,0 +1,10 @@
+var largestNumber = function(num) {
+    num.sort(function(a, b) {
+       var as = a.toString();
+       var bs = b.toString();
+       return as + bs > bs + as ? -1 : 1;
+    });
+    ans = num.join("");
+    while (ans[0] == "0" && ans.length > 1) ans = ans.substr(1);
+    return ans;
+};
