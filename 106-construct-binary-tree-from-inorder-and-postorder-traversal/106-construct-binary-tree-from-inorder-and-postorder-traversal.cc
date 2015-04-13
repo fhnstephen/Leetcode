a1cf9725b60994a6043bbf1bdf233c9e0961f90b
傅浩南 <fhnstephen@gmail.com>
2015-04-13 22:04:54 +0800

    Add 108.

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        return f(postorder.begin(), postorder.end(), inorder.begin(), inorder.end());
+    }
+private:
+    template<typename T>
+    TreeNode *f(T pb, T pe, T ib, T ie) {
+        if (pb >= pe || ib >= ie) return nullptr;
+        auto cur = *(pe - 1);
+        auto root = new TreeNode(cur);
+        auto i = ib;
+        auto size = 0;
+        while (i != ie && *i != cur) ++i, ++size;
+        root->left = f(pb, pb + size, ib, ib + size);
+        root->right = f(pb + size, pe - 1, i + 1, ie);
+        return root;
+    }
+};
