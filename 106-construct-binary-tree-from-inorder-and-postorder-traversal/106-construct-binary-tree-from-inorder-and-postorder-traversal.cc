5d49c0dcdecf8eca80575f48ca3dbb076c2186a5
傅浩南 <fhnstephen@gmail.com>
2015-04-13 22:12:35 +0800

    Update 106.

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cc
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -12,7 +12,7 @@ private:
         auto i = ib;
         auto size = 0;
         while (i != ie && *i != cur) ++i, ++size;
-        root->left = f(pb, pb + size, ib, ib + size);
+        root->left = f(pb, pb + size, ib, i);
         root->right = f(pb + size, pe - 1, i + 1, ie);
         return root;
     }
